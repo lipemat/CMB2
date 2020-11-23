493932647413ae4a603b84f7e685f1b9668da087
lipemat <mat@matlipe.com>
2020-11-23 16:19:29 -0700

    Bump to 2.7.0.2

--- init.php
+++ init.php
@@ -21,7 +21,7 @@
  *               Bill Erickson (@billerickson / billerickson.net)
  *               Andrew Norcross (@norcross / andrewnorcross.com)
  *
- * Version:      2.7.0.1
+ * Version:      2.7.0.2
  *
  * Text Domain:  cmb2
  * Domain Path:  languages
