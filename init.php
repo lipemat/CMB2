2552cc52250025e29ee542ab706ce38881e08ea1
lipemat <mat@matlipe.com>
2022-03-16 15:05:28 -0400

    Bump to 2.9.0.6

--- init.php
+++ init.php
@@ -21,7 +21,7 @@
  *               Bill Erickson (@billerickson / billerickson.net)
  *               Andrew Norcross (@norcross / andrewnorcross.com)
  *
- * Version:      2.9.0.5
+ * Version:      2.9.0.6
  *
  * Text Domain:  cmb2
  * Domain Path:  languages
@@ -75,7 +75,7 @@ if ( ! class_exists( 'CMB2_Bootstrap_290_Develop', false ) ) {
 		 * @var   string
 		 * @since 1.0.0
 		 */
-		const VERSION = '2.9.0.5';
+		const VERSION = '2.9.0.6';
 
 		/**
 		 * Current version hook priority.
