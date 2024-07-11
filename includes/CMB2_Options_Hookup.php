7286bea9aca7f6173d814faac58ef6c4776bd909
lipemat <mat@matlipe.com>
2024-07-11 11:25:42 -0400

    Add information about the settings when registering
    - label = The box label
    - description - Holds all the setting for the <id> options page.

--- includes/CMB2_Options_Hookup.php
+++ includes/CMB2_Options_Hookup.php
@@ -1,16 +1,17 @@
 <?php
+
 /**
  * Handles hooking CMB2 forms/metaboxes into the post/attachement/user screens
  * and handles hooking in and saving those fields.
  *
- * @since  2.0.0
- *
- * @category  WordPress_Plugin
- * @package   CMB2
  * @author    CMB2 team
- * @license   GPL-2.0+
+ * @since     2.0.0
+ *
  * @link      https://cmb2.io
  *
+ * @license   GPL-2.0+
+ * @package   CMB2
+ * @category  WordPress_Plugin
  * @property-read string $option_key
  * @property-read string $object_type
  * @property-read CMB2   $cmb
@@ -20,23 +21,25 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 	/**
 	 * The object type we are performing the hookup for
 	 *
-	 * @var   string
 	 * @since 2.0.9
+	 * @var   string
 	 */
 	protected $object_type = 'options-page';
 
 	/**
 	 * Options page key.
 	 *
-	 * @var   string
 	 * @since 2.2.5
+	 * @var   string
 	 */
 	protected $option_key = '';
+
 
 	/**
 	 * Constructor
 	 *
 	 * @since 2.0.0
+	 *
 	 * @param CMB2   $cmb        The CMB2 object to hookup.
 	 * @param string $option_key Option key to use.
 	 */
@@ -44,6 +47,7 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 		$this->cmb = $cmb;
 		$this->option_key = $option_key;
 	}
+
 
 	public function hooks() {
 		if ( empty( $this->option_key ) ) {
@@ -63,25 +67,29 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 		}
 
 		// Register setting to cmb2 group.
-		register_setting( 'cmb2', $this->option_key );
+		register_setting( 'cmb2', $this->option_key, [
+			'label'       => $this->cmb->prop( 'title' ),
+			'description' => "Holds all the settings for the `{$this->option_key}` options page.",
+		] );
 
 		// Handle saving the data.
-		add_action( 'admin_post_' . $this->option_key, array( $this, 'save_options' ) );
+		add_action( 'admin_post_' . $this->option_key, [ $this, 'save_options' ] );
 
 		// Optionally network_admin_menu.
 		$hook = $this->cmb->prop( 'admin_menu_hook' );
 
 		// Hook in to add our menu.
-		add_action( $hook, array( $this, 'options_page_menu_hooks' ), $this->get_priority() );
+		add_action( $hook, [ $this, 'options_page_menu_hooks' ], $this->get_priority() );
 
 		// If in the network admin, need to use get/update_site_option.
 		if ( 'network_admin_menu' === $hook ) {
 			// Override CMB's getter.
-			add_filter( "cmb2_override_option_get_{$this->option_key}", array( $this, 'network_get_override' ), 10, 2 );
+			add_filter( "cmb2_override_option_get_{$this->option_key}", [ $this, 'network_get_override' ], 10, 2 );
 			// Override CMB's setter.
-			add_filter( "cmb2_override_option_save_{$this->option_key}", array( $this, 'network_update_override' ), 10, 2 );
+			add_filter( "cmb2_override_option_save_{$this->option_key}", [ $this, 'network_update_override' ], 10, 2 );
 		}
 	}
+
 
 	/**
 	 * Hook up our admin menu item and admin page.
@@ -92,10 +100,10 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 	 */
 	public function options_page_menu_hooks() {
 		$parent_slug = $this->cmb->prop( 'parent_slug' );
-		$title       = $this->cmb->prop( 'title' );
-		$menu_title  = $this->cmb->prop( 'menu_title', $title );
-		$capability  = $this->cmb->prop( 'capability' );
-		$callback    = array( $this, 'options_page_output' );
+		$title = $this->cmb->prop( 'title' );
+		$menu_title = $this->cmb->prop( 'menu_title', $title );
+		$capability = $this->cmb->prop( 'capability' );
+		$callback = [ $this, 'options_page_output' ];
 
 		if ( $parent_slug ) {
 			$page_hook = add_submenu_page(
@@ -120,11 +128,12 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 
 		if ( $this->cmb->prop( 'cmb_styles' ) ) {
 			// Include CMB CSS in the head to avoid FOUC.
-			add_action( "admin_print_styles-{$page_hook}", array( 'CMB2_Hookup', 'enqueue_cmb_css' ) );
+			add_action( "admin_print_styles-{$page_hook}", [ 'CMB2_Hookup', 'enqueue_cmb_css' ] );
 		}
 
 		$this->maybe_register_message();
 	}
+
 
 	/**
 	 * If there is a message callback, let it determine how to register the message,
@@ -136,54 +145,52 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 	 */
 	public function maybe_register_message() {
 		$is_options_page = self::is_page( $this->option_key );
-		$should_notify   = ! $this->cmb->prop( 'disable_settings_errors' ) && isset( $_GET['settings-updated'] ) && $is_options_page;
-		$is_updated      = $should_notify && 'true' === $_GET['settings-updated'];
-		$setting         = "{$this->option_key}-notices";
-		$code            = '';
-		$message         = __( 'Nothing to update.', 'cmb2' );
-		$type            = 'notice-warning';
+		$should_notify = ! $this->cmb->prop( 'disable_settings_errors' ) && isset( $_GET['settings-updated'] ) && $is_options_page;
+		$is_updated = $should_notify && 'true' === $_GET['settings-updated'];
+		$setting = "{$this->option_key}-notices";
+		$code = '';
+		$message = __( 'Nothing to update.', 'cmb2' );
+		$type = 'notice-warning';
 
 		if ( $is_updated ) {
 			$message = __( 'Settings updated.', 'cmb2' );
-			$type    = 'updated';
+			$type = 'updated';
 		}
 
 		// Check if parameter has registered a callback.
 		if ( $cb = $this->cmb->maybe_callback( 'message_cb' ) ) {
-
 			/**
 			 * The 'message_cb' callback will receive the following parameters.
 			 * Unless there are other reasons for notifications, the callback should only
 			 * `add_settings_error()` if `$args['should_notify']` is truthy.
 			 *
-			 * @param CMB2  $cmb The CMB2 object.
-			 * @param array $args {
-			 *     An array of message arguments
+			 * @param CMB2  $cmb                 The CMB2 object.
+			 * @param array $args                {
+			 *                                   An array of message arguments
 			 *
-			 *     @type bool   $is_options_page Whether current page is this options page.
-			 *     @type bool   $should_notify   Whether options were saved and we should be notified.
-			 *     @type bool   $is_updated      Whether options were updated with save (or stayed the same).
-			 *     @type string $setting         For add_settings_error(), Slug title of the setting to which
+			 * @type bool   $is_options_page     Whether current page is this options page.
+			 * @type bool   $should_notify       Whether options were saved and we should be notified.
+			 * @type bool   $is_updated          Whether options were updated with save (or stayed the same).
+			 * @type string $setting             For add_settings_error(), Slug title of the setting to which
 			 *                                   this error applies.
-			 *     @type string $code            For add_settings_error(), Slug-name to identify the error.
+			 * @type string $code                For add_settings_error(), Slug-name to identify the error.
 			 *                                   Used as part of 'id' attribute in HTML output.
-			 *     @type string $message         For add_settings_error(), The formatted message text to display
+			 * @type string $message             For add_settings_error(), The formatted message text to display
 			 *                                   to the user (will be shown inside styled `<div>` and `<p>` tags).
 			 *                                   Will be 'Settings updated.' if $is_updated is true, else 'Nothing to update.'
-			 *     @type string $type            For add_settings_error(), Message type, controls HTML class.
+			 * @type string $type                For add_settings_error(), Message type, controls HTML class.
 			 *                                   Accepts 'error', 'updated', '', 'notice-warning', etc.
 			 *                                   Will be 'updated' if $is_updated is true, else 'notice-warning'.
-			 * }
+			 *                                   }
 			 */
 			$args = compact( 'is_options_page', 'should_notify', 'is_updated', 'setting', 'code', 'message', 'type' );
 
 			$this->cmb->do_callback( $cb, $args );
-
 		} elseif ( $should_notify ) {
-
 			add_settings_error( $setting, $code, $message, $type );
 		}
 	}
+
 
 	/**
 	 * Display options-page output. To override, set 'display_cb' box property.
@@ -204,7 +211,14 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 				<h2><?php echo wp_kses_post( $this->cmb->prop( 'title' ) ); ?></h2>
 			<?php endif; ?>
 			<?php $this->options_page_tab_nav_output(); ?>
-			<form class="cmb-form" action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>" method="POST" id="<?php echo $this->cmb->cmb_id; ?>" enctype="multipart/form-data" encoding="multipart/form-data">
+			<form
+				class="cmb-form"
+				action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>"
+				method="POST"
+				id="<?php echo $this->cmb->cmb_id; ?>"
+				enctype="multipart/form-data"
+				encoding="multipart/form-data"
+			>
 				<input type="hidden" name="action" value="<?php echo esc_attr( $this->option_key ); ?>">
 				<?php $this->options_page_metabox(); ?>
 				<?php submit_button( esc_attr( $this->cmb->prop( 'save_button' ) ), 'primary', 'submit-cmb' ); ?>
@@ -212,6 +226,7 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 		</div>
 		<?php
 	}
+
 
 	/**
 	 * Display options-page Tab Navigation output.
@@ -226,11 +241,15 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 		?>
 		<h2 class="nav-tab-wrapper">
 			<?php foreach ( $tabs as $option_key => $tab_title ) : ?>
-				<a class="nav-tab<?php if ( self::is_page( $option_key ) ) : ?> nav-tab-active<?php endif; ?>" href="<?php menu_page_url( $option_key ); ?>"><?php echo wp_kses_post( $tab_title ); ?></a>
+				<a
+					class="nav-tab<?php if ( self::is_page( $option_key ) ) : ?> nav-tab-active<?php endif; ?>"
+					href="<?php menu_page_url( $option_key ); ?>"
+				><?php echo wp_kses_post( $tab_title ); ?></a>
 			<?php endforeach; ?>
 		</h2>
 		<?php
 	}
+
 
 	/**
 	 * Outputs the settings notices if a) not a sub-page of 'options-general.php'
@@ -249,6 +268,7 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 		}
 	}
 
+
 	/**
 	 * Gets navigation tabs array for CMB2 options pages which share the
 	 * same tab_group property.
@@ -258,7 +278,7 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 	 */
 	public function get_tab_group_tabs() {
 		$tab_group = $this->cmb->prop( 'tab_group' );
-		$tabs      = array();
+		$tabs = [];
 
 		if ( $tab_group ) {
 			$boxes = CMB2_Boxes::get_by( 'tab_group', $tab_group );
@@ -278,6 +298,7 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 		return apply_filters( 'cmb2_tab_group_tabs', $tabs, $tab_group );
 	}
 
+
 	/**
 	 * Display metaboxes for an options-page object.
 	 *
@@ -286,6 +307,7 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 	public function options_page_metabox() {
 		$this->show_form_for_type( 'options-page' );
 	}
+
 
 	/**
 	 * Save data from options page, then redirects back.
@@ -305,7 +327,6 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 			&& isset( $_POST['submit-cmb'], $_POST['action'] )
 			&& $this->option_key === $_POST['action']
 		) {
-
 			$updated = $this->cmb
 				->save_fields( $this->option_key, $this->cmb->object_type(), $_POST )
 				->was_updated(); // Will be false if no values were changed/updated.
@@ -317,6 +338,7 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 		exit;
 	}
 
+
 	/**
 	 * Replaces get_option with get_site_option.
 	 *
@@ -324,11 +346,13 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 	 *
 	 * @param mixed $test    Not used.
 	 * @param mixed $default Default value to use.
+	 *
 	 * @return mixed Value set for the network option.
 	 */
 	public function network_get_override( $test, $default = false ) {
 		return get_site_option( $this->option_key, $default );
 	}
+
 
 	/**
 	 * Replaces update_option with update_site_option.
@@ -337,23 +361,27 @@ class CMB2_Options_Hookup extends CMB2_Hookup {
 	 *
 	 * @param mixed $test         Not used.
 	 * @param mixed $option_value Value to use.
+	 *
 	 * @return bool Success/Failure
 	 */
 	public function network_update_override( $test, $option_value ) {
 		return update_site_option( $this->option_key, $option_value );
 	}
 
+
 	/**
 	 * Determines if given page slug matches the 'page' GET query variable.
 	 *
 	 * @since  2.4.0
 	 *
-	 * @param  string $page Page slug.
+	 * @param string $page Page slug.
+	 *
 	 * @return boolean
 	 */
 	public static function is_page( $page ) {
 		return isset( $_GET['page'] ) && $page === $_GET['page'];
 	}
+
 
 	/**
 	 * Magic getter for our object.
